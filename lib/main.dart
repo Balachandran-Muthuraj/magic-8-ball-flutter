58eb4f0b53827c13e3bcd4e1e630a03e3c732271
Bala <rmbalachandar@gmail.com>
2020-06-21 23:21:01 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: BallPage(),
+        home: Ball(),
       ),
     );
 
-class BallPage extends StatelessWidget {
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballnumber=1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,6 +26,22 @@ class BallPage extends StatelessWidget {
           style: TextStyle(color: Colors.white),
         ),
       )),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              child: Center(
+                  child: FlatButton(child: Image.asset('images/ball$ballnumber.png'),onPressed: (){
+                   // print('Ball Clicked');
+                    setState(() {
+                      ballnumber =Random().nextInt(5)+1;
+                    });
+                  },)),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
